16dc3a4ace0b7c9b141e3c3cc14f15c0817b1a17
root <root@devopsadmin.xh0qisij20huhhy3ethvwo250b.cx.internal.cloudapp.net>
2021-01-21 04:56:14 +0000

    05-Tomcat

---
+++ 06-Terraform/10-LocalExec_with_Ansible/variables.tf
@@ -0,0 +1,11 @@
+variable "key_name" {
+  default = "terraform4key"
+}
+
+variable "pvt_key" {
+  default = "/root/.ssh/terraform-key.pem"
+}
+
+variable "sg_id" {
+  default = "sg-07cdd42313a3fd033"
+}
